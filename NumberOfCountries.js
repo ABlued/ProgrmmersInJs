bb938758c81a204d120943aa7e7a65f5356c5f3a
ablued <skwodhkswkd@naver.com>
2021-01-24 00:17:45 +0900

    124 나라의 숫자 문제 해결

---
+++ NumberOfCountries.js
@@ -0,0 +1,17 @@
+function solution() {
+    var answer = '';
+    const numbers = ["4","1","2"];
+    let n = parseInt(prompt("자연수를 입력하세요"));
+    let rest;
+    while(n > 0){
+        rest = parseInt(n % 3);
+        n = parseInt(n/3);
+        if(rest == 0) n--;
+        answer = numbers[rest] + answer;
+    }
+
+
+    alert(answer);
+    return answer;
+}
+solution();
