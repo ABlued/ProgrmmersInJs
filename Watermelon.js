6bf3816a09d8a86a705ac0cca13291d793a7e66b
ablued <skwodhkswkd@naver.com>
2021-02-12 00:52:03 +0900

    수박 문제 해결

---
+++ Watermelon.js
@@ -0,0 +1,10 @@
+const n = 4;
+function solution(n) {
+    var answer = '';
+    for(let i = 0; i < n; i++){
+        answer += i % 2 == 0 ? "수": "박";
+    }
+    return answer;
+}
+
+console.log(solution(n));
